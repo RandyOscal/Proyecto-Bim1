83e80905cbe4d329c83106ad1e97bf0c65914e20
RandyOscal <roscal-2023279@kinal.edu.gt>
2025-03-05 00:52:12 -0600

    Documentacion de swagger

--- src/auth/auth.routes.js
+++ src/auth/auth.routes.js
@@ -4,8 +4,54 @@ import { registerValidator, loginValidator } from "../middlewares/user-validator
 
 const router = Router()
 
+/**
+ * @swagger
+ * /register:
+ *   post:
+ *     summary: Register a new user
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               username:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: User registered successfully
+ *       400:
+ *         description: Bad request
+ */
 router.post("/register", registerValidator, register)
 
+/**
+ * @swagger
+ * /login:
+ *   post:
+ *     summary: Login a user
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               username:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: User logged in successfully
+ *       401:
+ *         description: Unauthorized
+ */
 router.post("/login", loginValidator, login)
 
 export default router
