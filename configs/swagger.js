40426039c754d58f6aff100ea98398f44187dc83
RandyOscal <roscal-2023279@kinal.edu.gt>
2025-03-05 15:38:28 -0600

    Se arreglo el archivo de swagger

--- configs/swagger.js
+++ configs/swagger.js
@@ -21,7 +21,10 @@ const swaggerOptions = {
     },
     apis:[
         "./src/auth/*.js",
-        "./src/user/*.js"
+        "./src/user/*.js",
+        "./src/category/*.js",
+        "./src/product/*.js"
+
 
     ]
 }
